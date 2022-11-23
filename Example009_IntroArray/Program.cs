a197039c97d18ed45c7cff8e4399cd9062aa2c20
Kirill <ad777miral@gmail.com>
2022-11-23 20:15:48 +0300

    IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int resalt = arg1;
+    if(arg2 > resalt) resalt = arg2;
+    if(arg3 > resalt) resalt = arg3;
+    return resalt;
+}
+
+int [] array = {23,31,-67,-100,-800,690,510,260,1000,132};
+
+int resalt = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(resalt);
