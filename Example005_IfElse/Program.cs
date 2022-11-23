f2aabe771671d27ff9bf7dc8744d174b30df76de
Kirill <ad777miral@gmail.com>
2022-11-23 14:47:21 +0300

    If_Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "люся")
+{
+    Console.WriteLine("Ура, это же Люся!!!");
+}
+else
+{
+    Console.Write("Привет! ты пидор - ");
+    Console.WriteLine(username);
+}
