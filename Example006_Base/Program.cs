171c7b78055559ce8c7bd85ebc8e910a5f1980d4
Kirill <ad777miral@gmail.com>
2022-11-23 15:31:33 +0300

    Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 5;
+int c = -8;
+int d = 100;
+int e = 20;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("Максимальное число равно - ");
+Console.WriteLine(max);
+
