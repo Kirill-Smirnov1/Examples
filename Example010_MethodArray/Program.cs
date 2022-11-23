dace6b48e775191d215bb332aa475a09bc0f87ba
Kirill <ad777miral@gmail.com>
2022-11-23 20:46:39 +0300

    MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {23,31,-67,-100,-800,690,510,260,1000,-800};
+int n = array.Length;
+int find = -800;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        //break;
+    }
+    index++;
+}
